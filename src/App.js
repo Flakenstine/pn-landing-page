df194ced8a8ea4bd011bdc92e31e797628b8ad76
Isaac Goodwin(Flakenstine) <isaacgoodwin97@gmail.com>
2021-03-07 20:01:26 -0700

    More changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './images/pn-logo.png';
 import logo_alt from './images/pn-full.png';
 import './styles/layout.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+  ButtonToggle
+} from 'reactstrap';
+import '@fontsource/open-sans';
 
 
-const App = () => {
+const items = [
+  {
+    title: 'Be our guest',
+    caption: 'Experience the magic. 24/7. Non stop.'
+  },
+  {
+    title: 'Step into the Magic',
+    caption: 'The number one destination for an all-inclusive family friendly experience.'
+  }
+];
+
+
+const App = (props) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  }
+
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img src={item.src} alt={item.altText} />
+        <CarouselCaption captionText={item.caption} captionHeader={item.title} />
+      </CarouselItem>
+    );
+  });
+
   return (
-    <div>
+    <div className="wrapper">
+      <div className="video-background">
+        <div classNam="video-wrap">
+          <div id="video">
+            <video id="bgvid" autoPlay loop muted playsInline>
+              <source src="https://cdn.palace.network/landing-page/background-video-1.mp4" type="video/mp4" />
+            </video>
+          </div>
+        </div>
+      </div>
       <header className="header">
         <div className="header-branding">
           <img src={logo} alt="PN Logo" />
@@ -21,11 +87,22 @@ const App = () => {
         </nav>
       </header>
       <main className="content">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora eligendi tenetur, harum nesciunt itaque dolorem eos in deserunt, ad laudantium saepe alias est possimus sint ipsa neque corporis laboriosam deleniti!
+        <Carousel
+        activeIndex={activeIndex}
+        next={next}
+        previous={previous}
+      >
+            {slides}
+      </Carousel>
+      <div className="server-status-card">
+        <h1>Come experience the magic today</h1>
+        <p><b>150</b> players online</p>
+        <button className="btn btn-primary">play.palace.network</button>
+      </div>
       </main>
       <footer className="footer">
         <div className="footer-branding">
-          <img src={logo_alt} />
+          <img src={logo_alt} alt="PN Logo full" />
         </div>
         <div className="footer-nav">
           <ul>
@@ -38,11 +115,12 @@ const App = () => {
         <div className="footer-nav-lower">
           <div className="footer-locale-selector">English</div>
           <div className="footer-social-links">
-            <a>Facebook</a>
-            <a>Twitter</a>
-            <a>Instagram</a>
-            <a>Youtube</a>
+            <a><FontAwesomeIcon icon={faFacebookF} /></a>
+            <a><FontAwesomeIcon icon={faTwitter} /></a>
+            <a><FontAwesomeIcon icon={faYoutube} /></a>
+            <a><FontAwesomeIcon icon={faInstagram} /></a>
           </div>
+          <div className="footer-copyright">&copy; {new Date().getFullYear()} Palace Interactive, Inc.</div>
         </div>
       </footer>
     </div>
